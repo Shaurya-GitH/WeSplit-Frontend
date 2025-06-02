429f9e08e21b65554a2efdffbb2340e0d66a7a79
Shaurya-GitH <mehta.shaurya123@gmail.com>
2025-06-02 13:47:08 +0530

    completed HomePage and HomeBar

--- src/components/HomeBar.jsx
+++ src/components/HomeBar.jsx
@@ -2,14 +2,14 @@ import {Link} from "react-router-dom";
 
 const HomeBar=()=>{
     return(
-        <header className={"flex sticky justify-end bg-blue-300 p-3"}>
-            <Link to="/" className="mr-auto text-white font-bold text-2xl ">
+        <header className={"flex sticky top-0 justify-end bg-gradient-to-r from-blue-50 to-indigo-50 p-3"}>
+            <Link to="/" className="mr-auto text-black font-bold text-2xl ">
                 Home
             </Link> <br/>
-            <Link to="/login" className="mx-3 text-white font-bold text-2xl">
+            <Link to="/login" className="mx-3 text-black font-bold text-2xl">
                 Login
             </Link> <br/>
-            <Link to="/register" className="mx-3 text-white font-bold text-2xl">
+            <Link to="/register" className="mx-3 text-blue-600 font-bold text-2xl">
                 Register
             </Link>
         </header>
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import App from './App.jsx'
 import { Provider } from "react-redux";
 import store from "./store.js";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import "./input.css"
 
 const queryClient= new QueryClient();
 createRoot(document.getElementById('root')).render(
